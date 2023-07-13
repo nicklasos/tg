11bf25b8a84b5b142218f2f6e32236f0397c043a
nicklasos <nicklasos@gmail.com>
2023-07-13 10:22:09 +0300

    *

--- commands/save.js
+++ commands/save.js
@@ -4,7 +4,10 @@ import {saveMessage} from "../src/messages.js";
 await saveMessages(async event => {
     let message = event.message;
 
-    console.log(message.senderId.toString(), message.text);
-
-    await saveMessage(message.senderId.toString(), message.text);
+    if (message.hasOwnProperty('senderId')) {
+        console.log(message.senderId.toString(), message.text);
+        await saveMessage(message.senderId.toString(), message.text);
+    } else {
+        console.log('Info: no senderId', message);
+    }
 });
