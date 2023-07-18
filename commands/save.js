df144efba5c896d39063d079ec8225fcfb3c5670
nicklasos <nicklasos@gmail.com>
2023-07-18 14:25:45 +0300

    Save all messages from channels

--- commands/save.js
+++ commands/save.js
@@ -1,11 +1,20 @@
 import {saveMessages} from "../src/save_messages.js";
 import {saveMessage} from "../src/messages.js";
+import * as fs from "fs";
 
 await saveMessages(async event => {
     let message = event.message;
 
-    if (message.hasOwnProperty('senderId')) {
-        console.log(message.senderId.toString(), message.text);
-        await saveMessage(message.senderId.toString(), message.text);
+    // console.dir(event, {depth: null});
+    // console.log(message);
+    // console.log(event.message);
+
+    // fs.writeFile('messages.txt', JSON.stringify(event.message) + "\n", { flag: "a+" }, (err) => {
+    //     if (err) console.error(err);
+    // });
+
+    if (message.hasOwnProperty('peerId') && message.peerId.hasOwnProperty('channelId')) {
+        console.log(message.peerId.channelId, message.text);
+        await saveMessage(message.peerId.channelId, message.text);
     }
 });
--- commands/t.js
+++ commands/t.js
@@ -1,9 +1,5 @@
 import db from "./../src/db.js";
 
-await db('messages').insert({
-    chat_id: '0',
-    message: 'test',
-});
 
 await db.destroy();
 
